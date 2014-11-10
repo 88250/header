ac27a7fb4c3b198b71d6eb855daf91d9b327b790
Liang Ding <dl88250@gmail.com>
2014-11-10 22:53:42 +0800

    .

--- main.go
+++ main.go
@@ -1,16 +1,103 @@
 package main
 
 import (
+	"bytes"
+	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
+	"sort"
+	"strings"
 	"text/template"
 )
+
+var header string
+
+type FileNode struct {
+	Path      string
+	Type      string // "f": file, "d": directory
+	FileNodes []*FileNode
+}
+
+func walk(path string, node *FileNode) {
+	files := listFiles(path)
+
+	for _, filename := range files {
+		fpath := filepath.Join(path, filename)
+
+		fio, err := os.Lstat(fpath)
+
+		checkErr("", err)
+
+		child := FileNode{Path: fpath, FileNodes: []*FileNode{}}
+		node.FileNodes = append(node.FileNodes, &child)
+
+		if fio.IsDir() {
+			child.Type = "d"
+
+			walk(fpath, &child)
+		} else {
+			child.Type = "f"
+
+			if !match(fpath, "TODO: pattern") {
+				continue
+			}
+
+			buf, err := ioutil.ReadFile(fpath)
+
+			checkErr("Reads file error: ", err)
+
+			log.Println(fpath)
+
+			//content := header + string(buf)
+
+			//fout, err := os.Create(fpath)
+			//checkErr("Prepare to write file err: ", err)
+
+			//fout.WriteString(content)
+
+			//err = fout.Close()
+			//checkErr("Write file err: ", err)
+
+			_ = buf
+		}
+	}
+}
+
+func listFiles(dirname string) []string {
+	f, _ := os.Open(dirname)
+
+	names, _ := f.Readdirnames(-1)
+	f.Close()
+
+	sort.Strings(names)
+
+	ret := []string{}
+
+	for _, name := range names {
+		fio, _ := os.Lstat(filepath.Join(dirname, name))
+
+		// TODO: excludes
+		if ".git" == fio.Name() {
+			continue
+		}
+
+		ret = append(ret, name)
+	}
+
+	return ret
+}
+
+func match(path, pattern string) bool {
+	return strings.HasSuffix(path, ".go")
+}
 
 func main() {
 	headerTemplate := "test-header.txt"
 
-	includes := []string{"**/xxx/*.go"}
+	dir := "."
 
+	includes := []string{"**/xxx/*.go"}
 	excludes := []string{"**/xxx/a.go"}
 
 	useDefaultExcludes := true
@@ -27,13 +114,23 @@ func main() {
 	_ = useDefaultExcludes
 	_ = mapping
 
+	dirPath, err := filepath.Abs(dir)
+	checkErr("Reads dir path error: ", err)
+
 	t, err := template.ParseFiles(headerTemplate)
+	checkErr("Can't find header template ["+headerTemplate+"]", err)
 
+	var buf bytes.Buffer
+	t.Execute(&buf, properties)
+	header = buf.String()
+
+	root := &FileNode{Path: dirPath, Type: "d", FileNodes: []*FileNode{}}
+	walk(dirPath, root)
+
+}
+
+func checkErr(errMsg string, err error) {
 	if nil != err {
-		log.Fatalf("Can't find header template [%s]", headerTemplate)
-
-		return
+		log.Fatal(errMsg, err)
 	}
-
-	t.Execute(os.Stdout, properties)
 }
