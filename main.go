c8b5b7365df34429ba2a2e95b3f131593a879659
Liang Ding <dl88250@gmail.com>
2014-11-12 22:37:59 +0800

    using shell file name pattern instead of antpath

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 // Copyright (c) 2014, B3log
-//  
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//  
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-//  
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -121,13 +121,19 @@ func match(path string) bool {
 	path = filepath.ToSlash(path)
 
 	for _, exclude := range conf.Excludes {
-		if AntPathMatch(exclude, path) {
+		match, err := filepath.Match(exclude, path)
+		checkErr("Exclude path match error", err)
+
+		if match {
 			return false
 		}
 	}
 
 	for _, include := range conf.Includes {
-		if AntPathMatch(include, path) {
+		match, err := filepath.Match(include, path)
+		checkErr("Include path match error", err)
+
+		if match {
 			return true
 		}
 	}
